4c5aaf1957e909b1f989f0fd8519c5283656f8af
mathias bigler <orchidevej@hotmail.com>
2018-05-16 14:16:31 +0100

    backend

--- Backend/src/main/java/backend/api/CarRemoteFetchFacade.java
+++ Backend/src/main/java/backend/api/CarRemoteFetchFacade.java
@@ -17,6 +17,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,101 +31,65 @@ import java.util.logging.Logger;
 public class CarRemoteFetchFacade {
     
     Gson gson = new Gson();
-    List<String> urls;
+    String[] urls = {
+        "https://stanitech.dk/carrentalapi/api/cars",
+        "http://www.ramsbone.dk:8085/api/cars"
+    };
     String baseURL = "https://stanitech.dk/carrentalapi/api/cars";
+    ExecutorService es = Executors.newFixedThreadPool(10);
+    List<Future<String>> list = new ArrayList<Future<String>>();
 
     public CarRemoteFetchFacade() {
-        urls = new ArrayList<>();
-        urls.add("https://stanitech.dk/carrentalapi/api/cars");
-        urls.add("http://www.ramsbone.dk:8081/api/cars");
     }
 
     private String fetch(String url) {
 
-        URL address;
         try {
-            address = new URL(url);
-        } catch (MalformedURLException ex) {
-            throw new NotFoundException("URL not found");
-        }
-        HttpURLConnection conn;
-        try {
-            conn = (HttpURLConnection) address.openConnection();
-        } catch (IOException ex) {
-            throw new NotFoundException("Unable to connect");
-        }
-
-        try {
+            URL address = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) address.openConnection();
             conn.setRequestMethod("GET");
-        } catch (ProtocolException ex) {
-            throw new NotFoundException("Internal error happened!");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("User-Agent", "server");
+            Scanner scan = new Scanner(conn.getInputStream());
+            String jsonStr = "";
+            while (scan.hasNext()) {
+                jsonStr += scan.nextLine();
+            }
+            scan.close();
+            return jsonStr;
         }
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("User-Agent", "server");
-
-        Scanner scan;
-        try {
-            scan = new Scanner(conn.getInputStream());
-        } catch (IOException ex) {
+        catch (Exception ex) {
             throw new NotFoundException("Unable to connect");
         }
-        String jsonStr = "";
-        while (scan.hasNext()) {
-            jsonStr += scan.nextLine();
-        }
-        scan.close();
-        return jsonStr;
     }
     
     private String put(String url, String message) {
 
-        URL address;
         try {
-            address = new URL(url);
-        } catch (MalformedURLException ex) {
-            throw new NotFoundException("URL not found");
-        }
-        HttpURLConnection conn;
-        try {
-            conn = (HttpURLConnection) address.openConnection();
+            URL address = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) address.openConnection();
             conn.setDoInput(true);
             conn.setDoOutput(true);
-        } catch (IOException ex) {
-            throw new NotFoundException("Unable to connect");
-        }
 
-        try {
             conn.setRequestMethod("PUT");
-        } catch (ProtocolException ex) {
-            throw new NotFoundException("Internal error happened!");
-        }
-        conn.setRequestProperty("Accept", "application/json");
-        conn.setRequestProperty("User-Agent", "server");
-        conn.setRequestProperty("Content-type", "application/json; charset=UTF-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("User-Agent", "server");
+            conn.setRequestProperty("Content-type", "application/json; charset=UTF-8");
         
-        OutputStream os;
-        try {
-            os = conn.getOutputStream();
+            OutputStream os = conn.getOutputStream();
             os.write(message.toString().getBytes("UTF-8"));
             os.close();
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(CarRemoteFetchFacade.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(CarRemoteFetchFacade.class.getName()).log(Level.SEVERE, null, ex);
-        }
         
-        Scanner scan;
-        try {
-            scan = new Scanner(conn.getInputStream());
+            Scanner scan = new Scanner(conn.getInputStream());
+            String jsonStr = "";
+            while (scan.hasNext()) {
+                jsonStr += scan.nextLine();
+            }
+            scan.close();
+            return jsonStr;
         } catch (IOException ex) {
             throw new NotFoundException("Unable to connect");
         }
-        String jsonStr = "";
-        while (scan.hasNext()) {
-            jsonStr += scan.nextLine();
-        }
-        scan.close();
-        return jsonStr;
         
     }
 
