e5fb456dd83bbe244e64b9fde6e887b32520e470
mellem <cph-ml474@cphbusiness.dk>
2018-05-17 20:12:14 +0200

    CarList handler if list is not an array

--- react-web/src/components/CarList.js
+++ react-web/src/components/CarList.js
@@ -4,13 +4,24 @@ import Car from './Car';
 export default class CarList extends React.Component {
      
     render() {
+        let list
+        let noMatch
 
-        const list = this.props.cars.map((car, index) => {
-            return <Car fromDate={this.props.fromDate} toDate={this.props.toDate} key={index} car={car} />
-        });
-        const noMatch = () => {
-            if (list.length === 0) return <p>Ingen resultater</p>
+        if(!Array.isArray(obj)) {
+            console.log('"this.props.cars" is not an array')
+            console.log('this.props.cars -> ' + JSON.stringify(this.props.cars))
+            list = <p> {'"this.props.cars" is not an array'} </p>
+            noMatch = () => {}
+        } else {
+            list = this.props.cars.map((car, index) => {
+                return <Car fromDate={this.props.fromDate} toDate={this.props.toDate} key={index} car={car} />
+            });
+            noMatch = () => {
+                if (list.length === 0) return <p>Ingen resultater</p>
+            }
         }
+
+        
 
         return (
             <div>
